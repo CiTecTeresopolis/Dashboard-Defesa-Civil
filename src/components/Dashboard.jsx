cf805dd35e0a09eab11359232ce1c7240bff7af7
Secretaria de Ciência e Tecnologia <cienciaetecnologia@teresopolis.rj.gov.br>
2025-10-21 15:26:21 -0300

    2017 adicionado

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -77,6 +77,7 @@ const Dashboard = () => {
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
+                  <SelectItem value="2017">2017</SelectItem>
                   <SelectItem value="2018">2018</SelectItem>
                   <SelectItem value="2019">2019</SelectItem>
                   <SelectItem value="2020">2020</SelectItem>
